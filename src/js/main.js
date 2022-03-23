a8f96250261613541ebea735d0ba9d62a42cfa07
Aimarest <amarcosestevez@gmail.com>
2022-03-23 13:45:00 +0100

    first js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('>> Ready :)');
+const form = document.querySelector('.js-form');
+const titleFill = document.querySelector('.js-titleFill');
+const titleShare = document.querySelector('.js-titleShare');
+const titleCard = document.querySelector('.js-titleCard');
+const secondFieldset = document.querySelector('.js-secondFieldset');
+const thirdFieldset = document.querySelector('.js-thirdFieldset');
+const fourFieldset = document.querySelector('.js-fourFieldset');
+const angle = document.querySelector('.js-angle');
+const angleShare = document.querySelector('.js-angleShare');
+
+titleFill.addEventListener('click',() => {
+ secondFieldset.classList.toggle('collapsed');
+ angle.classList.toggle('rotate');
+});
+
+titleShare.addEventListener('click',() => {
+thirdFieldset.classList.toggle('collapsed');
+angleShare.classList.toggle('rotate');
+});
+
+titleCard.addEventListener('click',() => {
+fourFieldset.classList.toggle('collapsed');
+});
